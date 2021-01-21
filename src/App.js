1fcefed3e1c0215c147585d905d6371bdcbe6b5f
surajsly <50519406+surajsly@users.noreply.github.com>
2021-01-21 21:34:35 +0530

    Task one done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [baseUrl, setbaseUrl] = useState(
+    "https://api.github.com/repos/angular/angular"
+  );
+  const [RepInfo, setRepInfo] = useState(null);
+
+  useEffect(async () => {
+    const response = await fetch(baseUrl);
+    const data = await response.json();
+    console.log(data);
+    setRepInfo(data);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>github issue page assignemet</h1>
+        <h2>{RepInfo?.full_name}</h2>
+        <h5>Open Issue{RepInfo?.open_issues}</h5>
       </header>
     </div>
   );
